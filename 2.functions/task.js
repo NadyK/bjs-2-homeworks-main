80ada7ae839f0df9b7764111fe8c6fd7c843c19d
Naan <naanko2021@gmail.com>
2022-11-11 13:10:20 +0300

    Выполнено укороченный вариант

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -19,12 +19,7 @@ avg = +(sum / arr.length).toFixed(2);
 
 // Задание 2
 function worker(arr) {
-  let sum;
-  sum = 0;
-     for (let i = 0; i < arr.length; i++) {
-      sum += arr[i];
-     }
-  return sum;
+    return arr.reduce((acc, current) => acc + current, 0)
 }
 
 function makeWork(arrOfArr, func) {
@@ -40,20 +35,5 @@ function makeWork(arrOfArr, func) {
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
-  let min, max;
-  sum = 0;
-  min = Infinity;
-  max = -Infinity;
-    for (let i = 0; i < arr.length; i++) {
-      sum += arr[i];
-      if (Math.abs(arr[i]) < min){
-          min = Math.abs(arr[i]);
-      }
-      if (Math.abs(arr[i]) > max){
-        max = Math.abs(arr[i]);
-      }
-    }
-  difference = max - min;
-  return difference;
+   return Math.abs(Math.max(...arr)-Math.min(...arr));
 }  
