5fbf3929b3dcfa78d5389c82afbc6b002de04cb4
Naan <naanko2021@gmail.com>
2022-11-14 23:37:01 +0300

    выполнено

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,17 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result
+  if (arr1.length !== arr2.length){
+   return result=false;
+  }
+  result=arr1.every((element, index) => element === arr2[index]);
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  let positiveNumbers = arr.filter(number => number > 0);
+  let multiplesOfThree = positiveNumbers.filter(numder => numder%3 == 0);
+  resultArr = multiplesOfThree.map(number => number*10);
+  
   return resultArr; // array
 }
