9e83a9801aa8596910c04f9ebeec36470166b7be
Naan <naanko2021@gmail.com>
2022-11-22 15:50:35 +0300

    первый

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -8,20 +8,99 @@ class PrintEditionItem {
   }
 
   fix(){
-    this.state = this.state * 1,5;
-  }
-
-  set state(curentState) {
-    if (curentState <= 0) {
-        this.state = 0;
+      this.state *=  1.5;
     }
-    if (curentState >= 100){
-        this.state = 100;
-    }
-    this.state = curentState;
-  }
 
-  get state(){
-    return this.state;
+  set state(сurrentState) {
+     if (сurrentState < 0) {
+         this._state = 0;
+       }
+     else if (сurrentState > 100){
+         this._state = 100;
+       } else {
+         this._state = сurrentState;
+       }
+    }
+
+  get state( ){
+    return this._state;
   }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor (name, releaseDate, pagesCount){
+        super(name, releaseDate, pagesCount)
+        this.type = "magazine";
+    }   
+}
+
+class Book extends PrintEditionItem {
+    constructor (author, name, releaseDate, pagesCount){
+        super(name, releaseDate, pagesCount)
+        this.type = "book";
+        this.author = author;
+    }   
+}
+
+class NovelBook extends Book {
+    constructor (author, name, releaseDate, pagesCount){
+        super(author, name, releaseDate, pagesCount)
+        this.type = "novel";
+    }   
+}
+
+class FantasticBook extends Book {
+    constructor (author, name, releaseDate, pagesCount){
+        super(author, name, releaseDate, pagesCount)
+        this.type = "fantastic";
+    }   
+}
+
+class DetectiveBook extends Book {
+    constructor (author, name, releaseDate, pagesCount){
+        super(author, name, releaseDate, pagesCount)
+        this.type = "detective";
+    }   
+}
+
+class Library{
+    constructor(name){
+      this.name =  name;
+      this.books = []; 
+    }
+   
+    //addBook(book){
+      //this.book = new PrintEditionItem;
+      //this.books.push(book)
+       
+     // if (this.book.state <= 30){
+      //   delete this.books(book);
+      // }
+   // }
+    addBook(book) {
+        this.book = new PrintEditionItem;
+        if (book.state > 30) {
+            this.books.push(book);
+        }
+    }
+
+    findBookBy(type, value){
+       this.type = type;
+       this.value = value;
+       if (this.value === this.book.name){
+          return this.book;
+       } else {
+          return null;
+       }
+       
+    }
+    
+    giveBookByName(bookName){
+       this.bookName = bookName;
+       if (bookName === this.book.name){
+        
+       }
+    }
+
+
 }
